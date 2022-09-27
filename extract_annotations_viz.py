00f6b3a5de0690cfaa7a986d5fe2184085ff49b7
Jakob McBroome <jakob@silverbullet.ucsc.edu>
2022-09-27 02:28:14 +0000

    fix miswrite

--- extract_annotations_viz.py
+++ extract_annotations_viz.py
@@ -3,5 +3,5 @@ import sys
 import pandas as pd
 t = bte.MATree(sys.argv[1])
 df = pd.read_csv(sys.argv[2],sep='\t')
-df['auto_annotation'] = df.strain.apply(lambda x:t.get_node(x).most_recent_annotations()[0])
+df['auto_annotation'] = df.strain.apply(lambda x:t.get_node(x).most_recent_annotation()[0])
 df.to_csv(sys.argv[3],sep='\t',index=False)
